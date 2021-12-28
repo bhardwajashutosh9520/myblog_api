378c372233a6c09f3733c13946d3b5d75f9b2de5
bhardwajashutosh9520 <bhardwajashutosh9520@gmail.com>
2021-12-28 20:42:52 +0530

    fix db connection

--- config/database.js
+++ config/database.js
@@ -1,16 +1,29 @@
 const mongoose = require('mongoose');
 
-mongoose
-  .connect(
-    'mongodb+srv://ashutosh9520:abblogdb@cluster0.gaylg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    },
-  )
-  .then(() => {
-    console.log('connection is established.');
-  })
-  .catch((err) => {
-    console.log('database connsection error-----', err);
-  });
+class Database {
+  static async connect(dbUrl = false) {
+    try {
+      if (process.env.NODE_ENV === 'development') {
+        // mongoose.set('debug', true);
+      }
+      await mongoose.connect(dbUrl || process.env.DB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      console.log('Database Connected', { time: new Date() });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  static async disconnect() {
+    try {
+      await mongoose.connection.close();
+      console.info('Database Disconnected', { time: new Date() });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+}
+
+module.exports = Database;
